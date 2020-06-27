c03fbc1918cb7d74b7c325451acb72b301f5bf6d
Will McLellarn <WillMcLellarn@gmail.com>
2020-06-27 13:48:32 -0700

    tictactoe bonuses. fun. not bad.

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -10,6 +10,8 @@ end
 def display_board(brd)
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts "First to 5 wins the game."
+
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -47,8 +49,14 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+  if find_winning_square(brd)
+    brd[find_winning_square(brd)] = COMPUTER_MARKER
+  elsif find_at_risk_square(brd)
+    brd[find_at_risk_square(brd)] = COMPUTER_MARKER
+  else
+    square = empty_squares(brd).sample
+    brd[square] = COMPUTER_MARKER
+  end
 end
 
 def board_full?(brd)
@@ -68,7 +76,7 @@ def detect_winner(brd)
     if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
        brd[line[2]] == PLAYER_MARKER
-      return 'Player'
+       return 'Player'
     elsif brd[line[0]] == COMPUTER_MARKER &&
           brd[line[1]] == COMPUTER_MARKER &&
           brd[line[2]] == COMPUTER_MARKER
@@ -101,28 +109,78 @@ def joinor(arr, separator = ", ", ending = "or")
   end
 end
 
+def display_score(scores)
+  puts "Player score is #{scores.count('Player')}. Computer score is #{scores.count('Computer')}. "
+  return [scores.count('Player'),scores.count('Computer')].max
+end
+
+def find_at_risk_square(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]] # diagonals
+  open_space = nil
+  winning_lines.each do |line|
+    counter = 0
+    line.each do |num|
+      if brd[num] == PLAYER_MARKER
+        counter += 1
+      end
+    end
+    if counter == 2
+      open_space = line.select {|num| brd[num] == " "}[0]
+    end
+  end
+  open_space
+end
+
+def find_winning_square(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]] # diagonals
+  open_space = nil
+  winning_lines.each do |line|
+    counter = 0
+    line.each do |num|
+      if brd[num] == COMPUTER_MARKER
+        counter += 1
+      end
+    end
+    if counter == 2
+      open_space = line.select {|num| brd[num] == " "}[0]
+    end
+  end
+  open_space
+end
+
 # Begin program run
 loop do
-  board = initialize_board
-  display_board(board)
-
+  scores = []
   loop do
+    board = initialize_board
+    loop do
+      display_board(board)
+      display_score(scores)
+
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
     display_board(board)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board)
+      winning_string = detect_winner(board)
+      scores << winning_string
+      prompt "#{winning_string} won this round!"
+      sleep(1)
+    else
+      prompt "It's a tie!"
+    end
+  break if display_score(scores) >= 5
   end
 
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
