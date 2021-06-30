5b68a9b3946508bdfb567d6726bfd3aa083360c5
Will <willmclellarn@gmail.com>
2021-06-30 12:26:54 -0600

    3 easy problems into 109 dir

---
+++ 109_assesment/scratch.rb
@@ -0,0 +1,20 @@
+# def repeat(str, int)
+#   int.times do
+#     puts str
+#   end
+# end
+#
+# repeat('Hello', 3)
+
+
+# def is_odd?(num)
+#   num % 2 == 1
+# end
+#
+#
+# puts is_odd?(2)    # => false
+# puts is_odd?(5)    # => true
+# puts is_odd?(-17)  # => true
+# puts is_odd?(-8)   # => false
+# puts is_odd?(0)    # => false
+# puts is_odd?(7)    # => true
